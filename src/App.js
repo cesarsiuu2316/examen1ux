541187bd2a6bed62d42eea0299a85e33b2207db1
cesarsiuu2316 <cesar23augusto16@gmail.com>
2023-05-20 23:04:23 -0600

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import menu from './Components/menu';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <menu></menu>
         <a
           className="App-link"
           href="https://reactjs.org"
